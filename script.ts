49d47357dedf0ee7116f77e1c8a515a7082dcba1
azhar qureshi <qureishiofficial@gmail.com>
2024-07-23 19:25:56 +0500

    add new function in script.ts

--- script.ts
+++ script.ts
@@ -3,3 +3,8 @@ function greet(name: string) {
 }
 
 greet('Azhar qureshi');
+
+const isUserExist = (email: string) => {
+  const users = ['a.a', 'b.b'];
+  return users.find((user) => user == 'a.a') ? true : false;
+};
